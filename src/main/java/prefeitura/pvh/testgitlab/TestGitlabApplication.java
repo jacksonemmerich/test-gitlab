8f9f8d847c21c2b06c580b7435dbbc521d43ce2f
Jackson Emmerich <jacksonemmerich@gmail.com>
2023-03-14 12:46:21 -0400

    Initial commit

---
+++ src/main/java/prefeitura/pvh/testgitlab/TestGitlabApplication.java
@@ -0,0 +1,13 @@
+package prefeitura.pvh.testgitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prefeitura/pvh/testgitlab/TestGitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package prefeitura.pvh.testgitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
